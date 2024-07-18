bfe584cb45cb4f49675460b259480795b697f779
Camila Marquez <camila.marquez@applydigital.com>
2024-07-17 19:23:32 -0600

    first commit - first basic test TC 01

---
+++ cypress/e2e/pages/checkout.cy.js
@@ -0,0 +1,11 @@
+describe('Casa Ideas - pruebas', () => {
+
+beforeEach(() => {
+    cy.visit('/')
+    //cy.url().should('contain', spacesNewYork)
+    cy.acceptCookiesIfExists()
+   //entrar directo al login en una prueba e iniciar sesion https://www.casaideas-mexico.mx/login
+  });
+
+  
+})
