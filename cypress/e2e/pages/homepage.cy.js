e465a70975dae4904942fa651a389db2d90c5184
cami.mar <camimar03@gmail.com>
2024-07-18 13:44:39 -0600

    creating TC 03

--- cypress/e2e/pages/homepage.cy.js
+++ cypress/e2e/pages/homepage.cy.js
@@ -9,14 +9,18 @@ describe('Casa Ideas - Homepage test suite', () => {
     });
 
 
-it('TC-01: User is able to access to the site', () => {
+it('TC_01: User is able to access to the site', () => {
   cy.url().should('eql', 'https://www.casaideas-mexico.mx/')
 });
 
-it.only('TC-02: User is able to click in Login button', () => {
+it('TC_02: User is able to click in Login button', () => {
   login.isLoginButtonVisibleEnabled();
   login.clickLoginButton();
   cy.url().should('include', '/login');
+});
+
+it('TC_03: Valid sign up', () => {
+  login.clickLoginButton();
 });
 
 
