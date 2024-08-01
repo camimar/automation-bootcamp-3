21ed21f5184af0da145f1ece70ab106af43d0fb0
cami.mar <camimar03@gmail.com>
2024-08-01 14:32:36 -0600

    TCs 16 and 16-1 added, passed

--- cypress/e2e/components/cart.js
+++ cypress/e2e/components/cart.js
@@ -1,25 +1,45 @@
 class Cart {
 
-	elements = {
+	  elements = {
 
-	   	getAddToCartButton: () => cy.contains('button', ' Agregar al carro '),
+        getAddToCartButton: () => cy.contains('button', ' Agregar al carro '),
         getCartIcon: () => cy.get('cx-icon.cart'),
-		getCartModal: () => cy.get('.CartSidebarView'),
-		getCartItem: () => cy.get('.ci-cart-item')
+        getCartModal: () => cy.get('.CartSidebarView'),
+        getCartItem: () => cy.get('.ci-cart-item'),
+        getIncreaseQuantityButton: () => cy.get('button[aria-label="Add one more"]'),
+        getQuantityInput: () => cy.get('input[aria-label="Quantity"]'),
+        getDecreaseQuantityButton: () => cy.get('button[aria-label="Remove one"]'),
 
 }
 
-    addProductToCart() {
-        cy.contains('Carretilla de Metal').scrollIntoView().should('be.visible').parents('ci-product-card').within(() => {
-        cy.wait(1000); 
-        this.elements.getAddToCartButton().click({force: true});
-        });
-    }
+  addProductToCart() {
+    cy.contains('Carretilla de Metal').scrollIntoView().should('be.visible').parents('ci-product-card').within(() => {
+    cy.wait(1000); 
+      this.elements.getAddToCartButton().click({force: true});
+      });
+  }
 
   verifyProductIsInCart(){
     this.elements.getCartModal().should('be.visible');
     this.elements.getCartItem().contains('Carretilla de Metal').should('be.visible');
   }
+
+  increaseProductQuantity(times) {
+    for (let i = 0; i < times; i++) {
+      this.elements.getIncreaseQuantityButton(cy.wait(500)).click();
+    }
+  }
+
+  verifyProductQuantity(expectedQuantity) {
+    this.elements.getQuantityInput().should('have.value', expectedQuantity.toString());
+  }
+
+  decreaseProductQuantity(times) {
+    for (let i = 0; i < times; i++) {
+      this.elements.getDecreaseQuantityButton().click().wait(500); 
+    }
+  }
+
 
 }
 
--- cypress/e2e/pages/cart.cy.js
+++ cypress/e2e/pages/cart.cy.js
@@ -10,9 +10,24 @@ describe('Casa Ideas - pruebas', () => {
     });
 
 
-    it.only('TC_15: User is able to add product to cart', () => {
-        cart.addProductToCart();
-        cart.verifyProductIsInCart();
+    it('TC_15: User is able to add product to cart', () => {
+      cart.addProductToCart();
+      cart.verifyProductIsInCart();
     });
+
+    it('TC_16: Product Quantity: User is able to Increase Product Quantity in cart', () => {
+      cart.addProductToCart();
+      cart.increaseProductQuantity(4);
+      cart.verifyProductQuantity(5);
+    });
+
+    it('TC_16-1: Product Quantity: User is able to decrease Product Quantity in car', () => {
+        cart.addProductToCart();
+        cart.increaseProductQuantity(2); 
+        cart.verifyProductQuantity(3);
+        cart.decreaseProductQuantity(2); 
+        cart.verifyProductQuantity(1);
+    });
+
 
 });
